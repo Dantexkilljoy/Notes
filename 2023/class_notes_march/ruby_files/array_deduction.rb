841319486cf17b0f87e23e0d3cfb8f131d21070a
Dustin Doyle <doyledustin9@gmail.com>
2023-03-10 15:23:54 -0600

    initial

--- 2023/class_notes_march/ruby_files/array_deduction.rb
+++ 2023/class_notes_march/ruby_files/array_deduction.rb
@@ -2,13 +2,13 @@ grocery_list = ["milk", "eggs", "bread", "ice cream", "potatoes"]
 puts grocery_list
 last_item = grocery_list.pop
 puts "---"
-puts last_item
+puts last_item #potatoes
 puts "---"
 puts grocery_list
 puts "---"
 first_item = grocery_list.shift
 puts "---"
-puts first_item
+puts first_item #milk
 puts "---"
 puts grocery_list
 puts "---"
